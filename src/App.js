89c6d9801d3cde3719c27b6d36d4165b1d995693
Marcos Paulo dos Santos <marcos.dev.1990@gmail.com>
2023-08-24 16:56:58 -0300

    eventos-js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,27 @@
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const cadastro = [
-    {
-      id: 1,
-      nome: "Marcos",
-      idade: 33,
-      data_nascimento: "08/03/1990",
-      nome_da_mãe: " Marina",
-      nome_do_pai: "",
-    },
-    {
-      id: 2,
-      nome: "Kelly",
-      idade: 34,
-      data_nascimento: "01/07/1989",
-      nome_da_mãe: "Maria",
-      nome_do_pai: "Osvaldo",
-    },
-    {
-      id: 3,
-      nome: "Carol",
-      idade: 32,
-      data_nascimento: "05/08/1991",
-      nome_da_mãe: "Marta",
-      nome_do_pai: "Marcio",
-    },
-    {
-      id: 4,
-      nome: "Bruno",
-      idade: 30,
-      data_nascimento: "02/11/1993",
-      nome_da_mãe: " Ana",
-      nome_do_pai: "Ronaldo",
-    },
-    {
-      id: 5,
-      nome: "Paula",
-      idade: 27,
-      data_nascimento: "28/09/1996",
-      nome_da_mãe: "Thais",
-      nome_do_pai: "",
-    },
-  ];
+  const [numero, setNumero] = useState(0);
+  const valorfixo = 4;
+  const mudar = (e) => {
+    setNumero(JSON.parse(e.target.value));
+  };
 
+  const calcular = () => {
+    const resultado = numero + valorfixo;
+    alert(resultado);
+  };
+
+  function alerta() {
+    alert("Você conseguiu!");
+  }
   return (
-    <div className="App">
-      {
-        cadastro.map(item =>{
-          return(
-            <div className="container">
-            <div>Nome: {item.nome}</div>
-            <div>Idade: {item.idade}</div>
-            <div> Data de nascimento: {item.data_nascimento}</div>
-            <div>Nome do mãe: {item.nome_da_mãe}</div>
-            {item.nome_do_pai !== "" && <div>Nome do Pai: {item.nome_do_pai}</div>} <br />
-            </div>
-          );
-        })
-      }
+    <div>
+      <input type="number" onChange={mudar} />
+      <button onClick={calcular}>Somar</button> <br/> <br/>
 
-
+      <div onClick={alerta}><h1>Clique aqui </h1> </div>
+      
     </div>
   );
 }

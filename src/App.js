89e627d67f0dcb2694482ee10144a7be29580b20
Marcos Paulo dos Santos <marcos.dev.1990@gmail.com>
2023-08-24 17:42:21 -0300

    filter

--- src/App.js
+++ src/App.js
@@ -1,68 +1,95 @@
 import "./App.css";
 
 function App() {
-  const cadastro = [
+  const formulario = [
     {
-      id: 1,
       nome: "Marcos",
-      idade: 33,
-      data_nascimento: "08/03/1990",
-      nome_da_mãe: " Marina",
-      nome_do_pai: "",
+      sobrenome: "fernandes",
+      email: "marcos@teste.com.br",
+      habilitado: false,
     },
     {
-      id: 2,
-      nome: "Kelly",
-      idade: 34,
-      data_nascimento: "01/07/1989",
-      nome_da_mãe: "Maria",
-      nome_do_pai: "Osvaldo",
+      nome: "Bruna",
+      sobrenome: "alves",
+      email: "bruna@teste.com.br",
+      habilitado: true,
     },
     {
-      id: 3,
-      nome: "Carol",
-      idade: 32,
-      data_nascimento: "05/08/1991",
-      nome_da_mãe: "Marta",
-      nome_do_pai: "Marcio",
+      nome: "Camila",
+      sobrenome: "Oliveira",
+      email: "camila@teste.com.br",
+      habilitado: false,
     },
     {
-      id: 4,
-      nome: "Bruno",
-      idade: 30,
-      data_nascimento: "02/11/1993",
-      nome_da_mãe: " Ana",
-      nome_do_pai: "Ronaldo",
-    },
-    {
-      id: 5,
-      nome: "Paula",
-      idade: 27,
-      data_nascimento: "28/09/1996",
-      nome_da_mãe: "Thais",
-      nome_do_pai: "",
+      nome: "Cedric",
+      sobrenome: "Santos",
+      email: "cedric@teste.com.br",
+      habilitado: true,
     },
   ];
 
+  const filtrando = formulario.filter((itemFormulario) => {
+    return itemFormulario.habilitado === false;
+  });
+
+  
+
+  const mercado = [
+    {
+      fruta: "abacaxi",
+      limpeza: " sabão",
+      mistura: "tulipa",
+    },
+    {
+      fruta: "maçã",
+      limpeza: " detergênte",
+      mistura: "calabresa",
+    },
+    {
+      fruta: "abacaxi",
+      limpeza: "desenfetante",
+      mistura: "figado",
+    },
+  ];
+
+  const lista = mercado.filter((itemMercado) => {
+    return itemMercado.fruta === "abacaxi";
+  });
+  
   return (
     <div className="App">
-      {
-        cadastro.map(item =>{
-          return(
+      <div className="list-1">
+        {filtrando.map((item) => {
+          return (
             <div className="container">
-            <div>Nome: {item.nome}</div>
-            <div>Idade: {item.idade}</div>
-            <div> Data de nascimento: {item.data_nascimento}</div>
-            <div>Nome do mãe: {item.nome_da_mãe}</div>
-            {item.nome_do_pai !== "" && <div>Nome do Pai: {item.nome_do_pai}</div>} <br />
+              <div>{item.nome}</div>
+              <div>{item.sobrenome}</div>
+              <div>{item.email}</div>
+              <div>{item.habilitado}</div>
+              <br />
             </div>
           );
-        })
-      }
+        })}
 
-
+      </div>
+      <div className="list-2">
+        {lista.map((item) => {
+          return (
+            <div className="container">
+              <div>{item.fruta}</div>
+              <div>{item.limpeza}</div>
+              <div>{item.mistura}</div>
+              <br />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+  
+
+
+};
+
 
 export default App;
